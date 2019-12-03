6213cf2b9aebdb35cc51daa99aeafbf45f963c74
Rex Crusade <rexcrusade@RexCrusade.local>
2019-12-03 17:36:38 -0500

    Database is commuicating to server. Post & Get request is all set!
    Co-authored by: James Gary gmarchesi888@gmail.com

--- server.js
+++ server.js
@@ -14,20 +14,19 @@ const bodyParser = require('body-parser');
 // * creating a connection string to speak with database
 
 mongoose.connect('mongodb+srv://rella_db_chat:iFtMZb9.znUo!uxBTMqt@cluster0-zqzvp.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-
-  })
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(
     () => {
       console.log('Database is connected!');
-    })
+    },
+  )
 
-    // * create an error handler in the event database cannot connect
-    .catch((err) => {
-      console.log(`Cannot access database${err}`);
-    });
-
+// * create an error handler in the event database cannot connect
+  .catch((err) => {
+    console.log(`Cannot access database${err}`);
+  });
 
 
 // * creating a new instance of the Schema
@@ -41,39 +40,36 @@ const MessageThread = mongoose.model('Chat Logs', messageThread);
 
 // mongodb+srv://rella_db_chat:iFtMZb9.znUo!uxBTMqt@cluster0-zqzvp.mongodb.net/test?retryWrites=true&w=majority
 
-// * posting and saving msg to the database with error handler 
+// * posting and saving msg to the database with error handler
 
 app.use(bodyParser.json());
-// app.post('/addmessage', (req, res) =>{
-//   console.log('In the post message block!');
-//   const myData = new MessageThread(req.body);
-//   myData.save()
-//   .then(msg => {
-//     res.send('msg saved to the database');
-//   })
-//   .catch(err => {
-//     res.status(400).send('Whoopsies! Unable to save to the database.');
-//   });
-// });
+
+// * creating and saving message to the database
 
 app.post('/addmessage', (req, res) => {
-  //console.log(req.body);
-  const myData = MessageThread.create({messages:[req.body]}, (err, msgData) => {
-  if(err) return console.log('Whoopsies! Issue with querying data.');
-  console.log(msgData);
-  res.send(msgData);
-});
+  // console.log(req.body);
+  const myData = MessageThread.create({ messages: [req.body] }, (err, msgData) => {
+    if (err) return console.log('Whoopsies! Issue with querying data.');
+    console.log(msgData);
+    res.send(msgData);
+  });
 });
 
+app.get('/getmessage', (req, res) => {
+  MessageThread.find({}, (err, msgData) => {
+    if (err) return console.log('Whoopsies! Issue with querying data.');
+    res.send(msgData);
+  });
+});
 // app.use('/', (req, res) => {
 //   //res.sendFile(__dirname +'/index.html');
 //   res.send('Jello World!');
 // });
 
 
-// * catch-all error handler 
+// * catch-all error handler
 app.all('*', (req, res) => res.status(404).send('Whoopsies! Page not found!'));
-//app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
