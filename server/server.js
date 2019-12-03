4c40692fd16bf22d32ff21440b97c0abae06088f
Jason Seidler <jsonseidler@gmail.com>
2019-12-03 14:31:29 -0500

    basic websocket chat working
    Co-authored-by: Anna <Anna.svetlov@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
 
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  return res.send('connected');
+  return res.sendFile(path.resolve(__dirname, '../client/index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user has connected');
+  socket.on('click', (msg) => {
+    console.log('message:' + msg);
+    io.emit('click', msg);
+  })
+  socket.on('disconnect', () => {
+    console.log('a user has disconnected');
+  })
 });
 
 http.listen(PORT, () => {
