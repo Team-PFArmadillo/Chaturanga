441baec26c2fe24029d6c7d9a6a380c7a1f7e105
Jason Seidler <jsonseidler@gmail.com>
2019-12-04 10:46:17 -0500

    added onload to fetch last 10 messages
    Co-authored-by: Anna <anna.svetlov@gmail.com>

--- client/index.js
+++ client/index.js
@@ -1,11 +1,33 @@
+
+//make initial call to database to get the last 10 messages - Persistent Messages
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Function running on doc load')
+    fetch('/getmessage')
+    .then ((res) => res.json())
+    .then ((res) => {
+      //loop through last 10 and call createMessageElement to append them to the page
+      let num = 10;
+      if (res.length < 10){
+        num = res.length-1;
+      }
+      for (let i=num; i >= 0; i-=1) {
+        let { message, username, time } = res[res.length-1-i].messages[0];
+        createMessageElement(message, username, time);
+      }
+    })
+  })
+
+
 //initializes the socket connection
 const socket = io();
 //emits value when the send button is clicked
 function clicker() {
+  const tempDate = new Date();
+  const timeString = (tempDate.getMonth()+1)+'/'+tempDate.getDate() + ' ' + tempDate.getHours() + ":" + tempDate.getMinutes()
   socket.emit('click', { 
     username: document.getElementById("username").value, 
     message: document.getElementById("message").value,
-    time: Date.now()
+    time: timeString
   });
 }
 //socket listens for the click emitter from the server and appending to the page
@@ -20,17 +42,17 @@ function createMessageElement(messageBody, username, timeStamp){
   const messageBubble = document.createElement('div');
   messageBubble.classList.add('message-bubble');
   
-  const userElement = document.createElement(`span`);
-  userElement.classList.add('user')
-  messageBubble.appendChild(userElement);
-  userElement.innerHTML = username
-  
   const timeStampElement = document.createElement(`span`);
   timeStampElement.classList.add('time-stamp')
   messageBubble.appendChild(timeStampElement);
-  timeStampElement.innerHTML = timeStamp
+  timeStampElement.innerHTML = (timeStamp + '  ')
+
+  const userElement = document.createElement(`span`);
+  userElement.classList.add('user')
+  messageBubble.appendChild(userElement);
+  userElement.innerHTML = (username + ': ')
   
-  const messageText = document.createElement(`div`);
+  const messageText = document.createElement(`span`);
   messageText.classList.add('message-text')
   messageBubble.appendChild(messageText)
   messageText.innerHTML = messageBody
@@ -40,3 +62,5 @@ function createMessageElement(messageBody, username, timeStamp){
   
   chatScreen.scrollTop = chatScreen.scrollHeight
 }
+
+
